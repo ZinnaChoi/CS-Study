2c0a7f817b8ee318a8bc45930886986888fceac8
Zinna <choiyh11097@gmail.com>
2023-10-01 19:45:23 +0900

    [feat] 프로그래머스 Lv1 > 최소 직사각형

---
+++ prac/src/main/java/com/example/prac/PracApplication.java
@@ -0,0 +1,13 @@
+package com.example.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracApplication.class, args);
+	}
+
+}
---
+++ prac/src/test/java/com/example/prac/PracApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
